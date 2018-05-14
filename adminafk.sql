8ce18be765e06a9ff9914e745c4fd2e855fde884
vitanombre <37785089+vitanombre@users.noreply.github.com>
2018-05-14 11:45:44 +0200

    Update adminafk.sql

--- adminafk.sql
+++ adminafk.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 14 mai 2018 à 09:29
+-- Généré le :  lun. 14 mai 2018 à 09:45
 -- Version du serveur :  5.7.19
 -- Version de PHP :  5.6.31
 
@@ -57,6 +57,88 @@ INSERT INTO `configs` (`id`, `name`, `value`, `type`) VALUES
 (13, 'display_participants', '1', 'boolean'),
 (14, 'display_schedule', '1', 'boolean'),
 (15, 'display_stream', '1', 'boolean');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `log`
+--
+
+DROP TABLE IF EXISTS `log`;
+CREATE TABLE IF NOT EXISTS `log` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `action` varchar(3000) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  `created_by` varchar(3000) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `ip` varchar(3000) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `level` int(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `login_fail`
+--
+
+DROP TABLE IF EXISTS `login_fail`;
+CREATE TABLE IF NOT EXISTS `login_fail` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `login_try` varchar(3000) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `fail` varchar(3000) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `ip` varchar(3000) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `date` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `token`
+--
+
+DROP TABLE IF EXISTS `token`;
+CREATE TABLE IF NOT EXISTS `token` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `number` varchar(3000) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` date NOT NULL,
+  `end_at` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `token`
+--
+
+INSERT INTO `token` (`id`, `number`, `created_at`, `end_at`) VALUES
+(1, '', '2018-04-05', '2018-04-06');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `login` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `level` int(255) NOT NULL DEFAULT '2',
+  `created_at` datetime NOT NULL,
+  `created_by` varchar(3000) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `update_at` datetime NOT NULL,
+  `update_by` varchar(3000) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `last_login` datetime NOT NULL,
+  `last_ip` varchar(3000) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`login`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`login`, `password`, `level`, `created_at`, `created_by`, `update_at`, `update_by`, `last_login`, `last_ip`) VALUES
+('admin', '$2y$10$lGz.ChiP2zfaI0vWLNQUreTfiOldZ0aFdd/9OO5b.FHCebsGaup7.', 1, '2018-04-08 00:00:00', 'admin', '2018-05-05 13:51:29', 'admin', '2018-05-14 09:28:38', '127.0.0.1');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
