01a7553198eefda9ef56201467529fae0219fb6c
Ludovic Ortega <mastership@hotmail.fr>
2018-07-03 01:16:56 +0200

    Update V1.50

--- adminafk.sql
+++ adminafk.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 25 juin 2018 à 02:17
+-- Généré le :  lun. 02 juil. 2018 à 23:10
 -- Version du serveur :  5.7.19
 -- Version de PHP :  5.6.31
 
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS `configs` (
   `value` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `type` varchar(3000) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `configs`
@@ -46,23 +46,24 @@ INSERT INTO `configs` (`id`, `name`, `value`, `type`) VALUES
 (2, 'url_glyphicon', '', 'string'),
 (3, 'default_ebot_rules', 'Lyon e-Sport', 'string'),
 (4, 'default_ebot_pass', 'LES', 'string'),
-(8, 'default_ebot_ot_mmr', '3', 'integer'),
+(8, 'default_ebot_ot_mmr', '5', 'integer'),
 (9, 'default_ebot_ot_money', '16000', 'integer'),
 (10, 'toornament_api', '', 'string'),
 (11, 'toornament_client_id', '', 'string'),
 (12, 'toornament_client_secret', '', 'string'),
 (13, 'toornament_id', '', 'string'),
 (14, 'display_connect', '1', 'boolean'),
-(15, 'display_bracket', '1', 'boolean'),
-(16, 'display_participants', '1', 'boolean'),
-(17, 'display_schedule', '1', 'boolean'),
-(18, 'display_stream', '1', 'boolean'),
+(16, 'display_bracket', '1', 'boolean'),
+(17, 'display_participants', '1', 'boolean'),
+(18, 'display_schedule', '1', 'boolean'),
+(19, 'display_stream', '1', 'boolean'),
 (6, 'default_ebot_ot_status', '1', 'boolean'),
 (7, 'default_ebot_knife', '1', 'boolean'),
-(5, 'default_ebot_match_mmr', '15', 'integer'),
-(19, 'steam_api', '', 'string'),
-(20, 'steamid_api', '', 'string'),
-(21, 'display_vac_ban', '1', 'boolean');
+(5, 'default_ebot_match_mmr', '5', 'integer'),
+(20, 'steam_api', '', 'string'),
+(21, 'steamid_api', '', 'string'),
+(15, 'display_veto', '1', 'boolean'),
+(22, 'display_vac_ban', '1', 'boolean');
 
 -- --------------------------------------------------------
 
@@ -144,7 +145,33 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`login`, `password`, `level`, `created_at`, `created_by`, `update_at`, `update_by`, `last_login`, `last_ip`) VALUES
-('admin', '$2y$10$lGz.ChiP2zfaI0vWLNQUreTfiOldZ0aFdd/9OO5b.FHCebsGaup7.', 1, '2018-04-08 00:00:00', 'admin', '2018-05-05 13:51:29', 'admin', '2018-06-19 19:14:18', '127.0.0.1');
+('admin', '$2y$10$lGz.ChiP2zfaI0vWLNQUreTfiOldZ0aFdd/9OO5b.FHCebsGaup7.', 1, '2018-04-08 00:00:00', 'admin', '2018-05-05 13:51:29', 'admin', '2018-07-02 16:47:47', '127.0.0.1');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `veto`
+--
+
+DROP TABLE IF EXISTS `veto`;
+CREATE TABLE IF NOT EXISTS `veto` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `date` datetime NOT NULL,
+  `team_1` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `team_2` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `format` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `mode` varchar(500) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `de_dust2` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `de_cache` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `de_mirage` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `de_overpass` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `de_nuke` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `de_cobblestone` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `de_train` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `de_inferno` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `ban_order` varchar(3000) CHARACTER SET utf32 COLLATE utf32_unicode_ci DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
